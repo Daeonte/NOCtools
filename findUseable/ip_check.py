1faca2276639be7ff7fc1db8c9c0bba1e93af88c
B. Kinard <bkinard@ad-user-ads-01.s.uw.edu>
2017-01-20 19:58:22 -0800

    Add default gateway variable to script

--- findUseable/ip_check.py
+++ findUseable/ip_check.py
@@ -6,8 +6,8 @@ exceptions
 drop sub from find_useable into module *
 snstatus on sub *
 grep subnet line *
-regex subnet line to /xx
-convert xx to int 
+regex subnet line to /xx *
+convert xx to int *
 subtract 32 from int
 2 ^ difference to give useable
 
@@ -15,8 +15,6 @@ for each useable reserve first and last
 check: if 256 useable -> reserve .100-.103
 for each useable range reserve first next 4 after first
 
-useful below
-subi 10.146.177.1 | sed '3q;d' | awk '{print $1}'
 """
 
 import subprocess
@@ -29,10 +27,15 @@ def CIDR(sub):
 
 	cidrNotation = re.compile('([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})(.*$)')
 	subnetMask = cidrNotation.search(findSubnet).group(5)
-	firstThreeOctets = cidrNotation.search(findSubnet).group(1,2,3)
+	firstThreeOctets = cidrNotation.search(findSubnet).group(1,2,3,4)
 	return firstThreeOctets, subnetMask[-2:]
 
 sub = sys.argv[1]
-firstThree, subnet = CIDR(sub)
-print firstThree
-print subnet
+firstFour, subnet = CIDR(sub)
+
+if (subnet == '24'):
+	defaultGateway = firstFour[0] + '.' + firstFour[1] + '.' + firstFour[2] + '.100'
+else:
+	lastOctet = int(firstFour[3]) + 1
+	defaultGateway = firstFour[0] + '.' + firstFour[1] + '.' + firstFour[2] + '.' + str(lastOctet)
+print defaultGateway
