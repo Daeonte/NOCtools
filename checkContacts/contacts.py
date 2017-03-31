99ca4689c68707a20b08863aef65a9b602c1513e
brandondakinard <brandondakinard@gmail.com>
2017-03-31 12:23:09 -0700

    Update contacts.py to account for internal tool change

--- checkContacts/contacts.py
+++ checkContacts/contacts.py
@@ -11,11 +11,9 @@ contact = sys.argv[2]
 
 callConi = subprocess.Popen(('coni', fqdn), stdout=subprocess.PIPE)
 
-checkHead = subprocess.Popen('head -11', stdin=callConi.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
+checkHead = subprocess.Popen('head -7', stdin=callConi.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
 
-callConi = subprocess.Popen(('coni', fqdn), stdout=subprocess.PIPE)
+checkName = subprocess.Popen('grep ' + contact + ' -B 3 -A 4', stdin=callConi.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
 
-checkName = subprocess.Popen(('grep', contact, '-A 7'), stdin=callConi.stdout, stdout=subprocess.PIPE).communicate()[0]
-
-print checkHead + checkName
+print checkHead + '\n' + checkName
 
